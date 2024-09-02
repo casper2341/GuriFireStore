1e4398ba5e2edbde7c6ecc116ddefb8a00e241f5
Gurdeep <saini.gurdeep1688@gmail.com>
2024-09-03 00:24:23 +0530

    adding some libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.kotlinx.coroutines)
+    implementation(libs.androidx.lifecycle.viewmodel)
 }
